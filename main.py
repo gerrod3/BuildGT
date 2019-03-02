156a262d3bb7c6a1254fc35eb1b86453ac1ce2c7
Sudhanshu Agarwal <sudhanshu@lawn-143-215-56-146.lawn.gatech.edu>
2019-03-02 18:37:12 -0500

    Traffic Light detection works

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import numpy as np
 import cv2
 
 from TrafficLight import findLight
-#from Led import start
-#from Led import stop
 
 video = cv2.VideoCapture('traffic.mp4')
 success,image = video.read()
@@ -12,16 +10,26 @@ count = 0
 
 show = False
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(15,GPIO.OUT)
+GPIO.setup(18,GPIO.OUT)
+
 while success:
-    #start()
     cv2.imwrite("test.jpg", image)
     result, modImage = findLight("red")
     if result :
         print("RED")
-        #stop()
+        state = GPIO.input(18)
+        if state == False:
+            GPIO.output(18,GPIO.HIGH)
+            GPIO.output(15,GPIO.LOW)
     else :
         print("NONE")
-        #start()
+        state = GPIO.input(15)
+        if state == False:
+            GPIO.output(15,GPIO.HIGH)
+            GPIO.output(18,GPIO.LOW)
     count = count + 1
 
     success, image = video.read()
@@ -30,5 +38,7 @@ while success:
         if cv2.waitKey(33) == 27:
             break
 
+GPIO.output(15,GPIO.LOW)
+GPIO.output(18,GPIO.LOW)
 video.release()
 cv2.destroyAllWindows()
