54cf430ea4b1ade5c45a9221908206506aa85819
Sudhanshu Agarwal <sudhanshu@lawn-143-215-56-146.lawn.gatech.edu>
2019-03-02 15:00:24 -0500

    Minor update

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ import sys
 import numpy as np
 import cv2
 
-from TrafficLight import detect_traffic_lights
+from TrafficLight import findLight
 
 video = cv2.VideoCapture('traffic.mp4')
 success,image = video.read()
 count = 0
 while success:
-    if count % 50 == 0 :
-        cv2.imwrite("./test/img_%d.jpg" % (count / 50), image)
-    success, image = video.read()
+    cv2.imwrite("test.jpg", image)
+    if findLight() == True :
+        print("RED")å
+    else :
+        print("NONE")
     count = count + 1
-print(count)
-print(detect_traffic_lights("./test", 'faster_rcnn_resnet101_coco_11_06_2017', count, plot_flag=False))
+    success, image = video.read()
 video.release()
 cv2.destroyAllWindows()
