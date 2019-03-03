bd82dc0e1b3a7b4829a291f31b7b5b24ea378b5d
Sudhanshu Agarwal <sudhanshu@lawn-143-215-56-146.lawn.gatech.edu>
2019-03-02 21:32:05 -0500

    Changed GPIO pin

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ success,image = video.read()
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(15,GPIO.OUT)
+GPIO.setup(12,GPIO.OUT)
 GPIO.setup(18,GPIO.OUT)
 
 while success:
@@ -24,16 +24,16 @@ while success:
         state = GPIO.input(18)
         if state == False:
             GPIO.output(18,GPIO.HIGH)
-            GPIO.output(15,GPIO.LOW)
+            GPIO.output(12,GPIO.LOW)
     else :
         print("NONE")
-        state = GPIO.input(15)
+        state = GPIO.input(12)
         if state == False:
-            GPIO.output(15,GPIO.HIGH)
+            GPIO.output(12,GPIO.HIGH)
             GPIO.output(18,GPIO.LOW)
-            success, image = video.read()
+    success, image = video.read()
 
-GPIO.output(15,GPIO.LOW)
+GPIO.output(12,GPIO.LOW)
 GPIO.output(18,GPIO.LOW)
 video.release()
 cv2.destroyAllWindows()
